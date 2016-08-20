33df39dbb5362bd343bc93759590996620362f15
Peter Axelsson <peter@reducks.se>
2016-08-20 09:04:03 +0000

    if

--- get_temp.sh
+++ get_temp.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-if [-a /sys/bus/w1/devices/28*/w1_slave] THEN
-	
+if ls /sys/bus/w1/devices/28*/w1_slave &>/dev/null; then
 
 	# Get temps form w1
 	OutsideTemp=`cat /sys/bus/w1/devices/28*/w1_slave| grep -o 't=.*'|cut -f2- -d'='|awk '{$1=$1 / 1000;printf "%.2f", $1}'`
@@ -10,10 +9,9 @@ if [-a /sys/bus/w1/devices/28*/w1_slave] THEN
 	SensorId=`ls /sys/bus/w1/devices/28*/w1_slave|cut -f6 -d'/'`
 
 
-	# wget -q -O/dev/null "http://www.sjobyn.se/savedata.php?id=$SensorId&data=$OutsideTemp"
+	wget -q -O/dev/null "http://www.sjobyn.se/savedata.php?id=$SensorId&data=$OutsideTemp"
 
 	echo $SensorId  $OutsideTemp
-
-ELSE
+else
 	echo "No data found!"
-FI
+fi
