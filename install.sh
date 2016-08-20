7aabc43001a105d7b20901d710f6bbcaabd97ae4
PAx <peter@codeit.no>
2016-08-20 12:28:08 +0200

    install

--- install.sh
+++ install.sh
@@ -3,13 +3,15 @@
 sudo modprobe w1-gpio
 sudo modprobe w1-therm
 
+# sudo echo "dtoverlay=w1-gpio">>/boot/config.txt
+
 git config --global user.email "peter@reducks.se"
 git config --global user.name "Peter Axelsson"
 
 # Fix a cronjob
 sudo crontab -l > mycron
 echo "* * * * *           /home/pi/doglogg/get_temp.sh" >> mycron
-crontab mycron
+sudo crontab mycron
 
 
-echo "Done!"
+echo "Done! Reboot needed..."
