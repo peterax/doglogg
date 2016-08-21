9e6f52c32259e5fb6fae628af8100603b336d84b
PAx <peter@codeit.no>
2016-08-21 13:52:23 +0200

    qw

--- get_temp.sh
+++ get_temp.sh
@@ -5,7 +5,7 @@ if ls /sys/bus/w1/devices/28*/w1_slave &>/dev/null; then
 	# Get temps form w1
 	OutsideTemp=`cat /sys/bus/w1/devices/28*/w1_slave| grep -o 't=.*'|cut -f2- -d'='|awk '{$1=$1 / 1000;printf "%.2f", $1}'`
 
-	# Get sensor id
+	# Get sensors id
 	SensorId=`ls /sys/bus/w1/devices/28*/w1_slave|cut -f6 -d'/'`
 
 
