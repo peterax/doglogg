98e946e4173e327c3ac95052fa142958eacca20f
PAx <peter@codeit.no>
2016-08-21 14:28:36 +0200

    asdf

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ git config --global user.name "Peter Axelsson"
 # Fix a cronjob
 sudo crontab -l > mycron
 echo "* * * * *           /home/pi/doglogg/get_temp.sh" >> mycron
+echo "* * * * *           /home/pi/doglogg/gitpull.sh" >> mycron
 sudo crontab mycron
 
 
