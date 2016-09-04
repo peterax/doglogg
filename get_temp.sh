0a6301ea1566b208731d5d440e3829009851d8f6
Peter Axelsson <peter@reducks.se>
2016-09-04 09:41:04 +0000

    More than 1 sensor

--- get_temp.sh
+++ get_temp.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
-if ls /sys/bus/w1/devices/28*/w1_slave &>/dev/null; then
+# Set path
+thePath="/sys/bus/w1/devices/28*/w1_slave"
+
+# Get pi cpu serno
+cpuSerialNo=`cat /proc/cpuinfo | grep Serial | cut -d ':' -f 2 | cut -d ' ' -f 2`
+# Get pi hw revision
+hwRevision=`cat /proc/cpuinfo | grep Revision | cut -d ':' -f 2 | cut -d ' ' -f 2`
+
+# Any files?
+if ls $thePath &>/dev/null; then
+      
+# Loop them
+for filename in $thePath; do
 
 	# Get temps form w1
-	OutsideTemp=`cat /sys/bus/w1/devices/28*/w1_slave| grep -o 't=.*'|cut -f2- -d'='|awk '{$1=$1 / 1000;printf "%.2f", $1}'`
+	OutsideTemp=`cat $filename| grep -o 't=.*'|cut -f2- -d'='|awk '{$1=$1 / 1000;printf "%.2f", $1}'`
 
 	# Get sensors id
-	SensorId=`ls /sys/bus/w1/devices/28*/w1_slave|cut -f6 -d'/'`
-
-	# Get pi cpu serno
-	cpuSerialNo=`cat /proc/cpuinfo | grep Serial | cut -d ':' -f 2 | cut -d ' ' -f 2`
- 	# Get pi hw revision
-        hwRevision=`cat /proc/cpuinfo | grep Revision | cut -d ':' -f 2 | cut -d ' ' -f 2`
+	SensorId=`ls $filename|cut -f6 -d'/'`
 
 	# Save data
 	wget -q -O/dev/null "http://www.doglogg.se/doglogg_save.php?id=$SensorId&data=$OutsideTemp&user=$cpuSerialNo-$hwRevision"
 
-    # wget -q -O/dev/null  "http://api.thingspeak.com/update?api_key=RKDFIP1P1VM72FE7&field1=$OutsideTemp&field2=$SensorId"
+       # wget -q -O/dev/null  "http://api.thingspeak.com/update?api_key=RKDFIP1P1VM72FE7&field1=$OutsideTemp&field2=$SensorId"
 
+	# Just for the cause
 	echo $SensorId  $OutsideTemp
+done
+
 else
 	echo "No data found!"
 fi
