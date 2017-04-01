96acf333d8506c25640c4fc640cc1357541c3875
PAx <peter@codeit.no>
2017-04-01 12:13:32 +0200

    WPA_supplicant

--- install.sh
+++ install.sh
@@ -9,6 +9,17 @@ sudo cp config.txt /boot/config.txt
 rm config.txt
 # sudo echo "dtoverlay=w1-gpio">>/boot/config.txt
 
+
+# Open WLANs
+cp /etc/wpa_supplicant/wpa_supplicant.conf ./
+echo "network={" >> wpa_supplicant.conf
+echo "        key_mgmt=NONE" >> wpa_supplicant.conf
+echo "       priority=-999" >> wpa_supplicant.conf
+echo "}" >> wpa_supplicant.conf
+sudo cp wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+#rm wpa_supplicant.conf
+
+
 git config --global user.email "peter@reducks.se"
 git config --global user.name "Peter Axelsson"
 
